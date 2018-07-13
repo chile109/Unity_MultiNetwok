95bc22a06448a54924861eb9d8f70a546cdda213
Kevin Chang <kevin@megalive.com.tw>
2018-07-13 16:26:59 +0800

    [Add] Unity TCP client

---
+++ UnityTCP/Assets/NetworkManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+public class NetworkManager : MonoBehaviour
+{
+	public InputField Keyin;
+	public Button s_btn;
+	private static Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+	static IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse("10.211.55.3"), 100);
+	int attempts = 0;       //連線次數
+							// Use this for initialization
+	void Start()
+	{
+		LoopConnect();
+		//s_btn.interactable = false;
+		//ClientTCP tCP = new ClientTCP();
+		//tCP.Connect();
+	}
+
+	//嘗試發訊
+	public void Send()
+	{
+		string req = Keyin.text;
+		byte[] buffer = Encoding.ASCII.GetBytes(req);
+		_clientSocket.Send(buffer);     //發送request
+
+		byte[] receivedBuf = new byte[1024];
+		int rec = _clientSocket.Receive(receivedBuf);   //接收responce
+
+		byte[] data = new byte[rec];
+		Array.Copy(receivedBuf, data, rec);     //裁減responce
+		Debug.Log("Received: " + Encoding.ASCII.GetString(data));
+	}
+
+	//private void Update()
+	//{
+	//	if (_clientSocket.Connected)
+	//	{
+	//		try
+	//		{
+	//			attempts++;
+	//			_clientSocket.Connect(remoteEP);
+	//		}
+	//		catch (SocketException)
+	//		{
+	//			Debug.Log("Connection attemps: " + attempts.ToString());
+	//		}
+	//	}
+	//	else
+	//	{
+	//		s_btn.interactable = true;
+	//	}
+	//}
+
+	private static void LoopConnect()
+	{
+		int attempts = 0;       //連線次數
+
+		while (!_clientSocket.Connected)
+
+			try
+			{
+				attempts++;
+				_clientSocket.Connect(remoteEP);
+			}
+			catch (SocketException)
+			{
+				Debug.Log("Connection attemps: " + attempts.ToString());
+			}
+
+		Debug.Log("connected");
+	}
+
+}
---
+++ UnityTCP/Assets/TCPclient.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class ClientTCP
+{
+	public TcpClient client;
+	public NetworkStream myStream;
+	private byte[] asyncBuffer;
+	public bool isConnected;
+
+	private string IP_Adress = "10.211.55.3";
+	private int Port = 100;
+
+	public void Connect()
+	{
+		Debug.Log("try to connecting!");
+		client = new TcpClient();
+		client.ReceiveBufferSize = 4096;
+		client.SendBufferSize = 4096;
+		asyncBuffer = new byte[8192];
+
+		try
+		{
+			client.BeginConnect(IP_Adress, Port, new AsyncCallback(ConnectCallBack), client);
+		}
+		catch (Exception)
+		{
+			Debug.Log("Fail Connect");
+		}
+	}
+
+	private void ConnectCallBack(IAsyncResult result)
+	{
+		Debug.Log("callback");
+		try
+		{
+			Debug.Log("callback1");
+			client.EndConnect(result);
+			Debug.Log("callback2");
+			if (client.Connected == false) { return; }
+			else
+			{
+				myStream = client.GetStream();
+				myStream.BeginRead(asyncBuffer, 0, 8192, OnReceiveData, null);
+				isConnected = true;
+				Debug.Log("connect successful");
+			}
+
+		}
+		catch (Exception ex)
+		{
+			Debug.Log(ex);
+			isConnected = false;
+			return;
+		}
+	}
+
+	private void OnReceiveData(IAsyncResult result)
+	{
+		try
+		{
+
+		}
+		catch (Exception)
+		{
+			throw;
+		}
+	}
+}
+
